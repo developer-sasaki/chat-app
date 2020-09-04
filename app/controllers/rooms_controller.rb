efffdf63db2a4f7ff207fe589757c6901bc2c6d1
developer-sasaki <69506440+developer-sasaki@users.noreply.github.com>
2020-09-04 18:07:44 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,19 +1,20 @@
-def new
-  @room = Room.new
-end
-
-def create
-  @room = Room.new(room_params)
-  if @room.save
-    redirect_to root_path
-  else
-    render :new
+class RoomsController < ApplicationController
+  def new
+    @room = Room.new
   end
-end
 
-private
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-def room_params
-  params.require(:room).permit(:name, user_ids:[])
-end
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
